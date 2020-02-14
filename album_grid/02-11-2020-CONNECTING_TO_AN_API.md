44a37795b6e9edddb0819da14f9a70705e88b909
DakotaLMartinez <dakotaleemusic@gmail.com>
2020-02-13 17:50:02 -0800

    add updating feature to album grid

--- album_grid/02-11-2020-CONNECTING_TO_AN_API.md
+++ album_grid/02-11-2020-CONNECTING_TO_AN_API.md
@@ -8,7 +8,6 @@
   - [JSONVIEW Chrome Extension](https://chrome.google.com/webstore/detail/jsonview/chklaanhfefbnpoihckbnefhakgolnmc?hl=en)
 2. Connect to that server using fetch to load albums into the DOM
 3. Hook up our form so that it posts data to our JSON API
-4. (Maybe Thursday) Set up Editing and deleting as well.
 
 ## Concepts
 
@@ -36,3 +35,39 @@ Promise
 * async is when you're using your own individual timing
 * Javascript is single threaded
 * Browser can do multiple tasks at once
+
+# 02-13-2020 Study Group 
+## Tasks
+1. Add the ability to Edit an Existing Album 
+- PATCH request to /albums/:id
+- JS needs to know what the current role of the form is(new or edit) (editingAlbum state property)
+- Split up the code that sends requests into separate functions (createAlbum, updateAlbum)
+- If it's edit, it also needs to know which album we're editing (the id)
+- Our event listener is always creating a POST request. We need it to submit a post request when we're creating a new one, but to submit a PATCH to the right URL if we're editing.
+- Add in a variable to track the state of the form so our JS knows what should happen at any given point. (covertArtValue, albumTitleValue, aristNameValue, editingAlbum: null)
+- We need to add event listeners so that when users interact with the form, or click on an edit link, the formState updates.
+- create a function that loads the form with data from the formState object.
+
+### UI State
+Albums currently loaded from API
+formState (current state of the form fields)
+editingAlbumId: the id of the album we're editing (unless we're creating a new one in which case this will be null)
+### View Logic 
+function to display the form filled with values from the state
+logic to change header for album form if we're editing vs adding a new album
+Add an edit button to each album in the grid
+### Rerendering functions
+(where we hook in to the view logic and actually put updated info in the dom when the UI state changes)
+These functions get called from event handlers 
+### User Actions and how they update UI state
+When a user clicks on an edit button for an Album, we need to update the formState with that album's info and set editingAlbumId to that album's id.
+When a user submits the form, we need to check whether we're editing or creating and call the right function for that moment.
+After a user submits the form, we want to clear out the form fields (and reset formState)
+When a user clicks on the edit button, we should see the form to edit that album and only that Album in the grid.
+  - Add event listener to the edit button
+  - find the album in UIState.albums that the user wants to edit
+  - UIState.formState = editingAlbum
+  - UIState.editingAlbumId = editingAlbum.id
+  - updateAlbumList([editingAlbum])
+  - updateForm(UIState.formState)
+  - finish the updateAlbum function
--- album_grid/js/script.js
+++ album_grid/js/script.js
@@ -1,4 +1,35 @@
-let albums = []
+// UI state
+let UIState = {
+  albums: [],
+  formState: {
+    coverArt: '',
+    albumTitle: '',
+    artistName: ''
+  },
+  editingAlbumId: null
+}
+
+// view logic
+const renderForm = (formState) => {
+  return `
+    <form id="addAlbum">
+      <h3>${formState.id ? "Edit Album" : "Add Album"}</h3>
+      <p>
+        <label for="coverArt" class="db">Cover Art</label>
+        <input type="text" name="coverArt" id="coverArt" value="${formState.coverArt}" class="trackInput" />
+      </p>
+      <p>
+        <label for="title" class="db">Album Title</label>
+        <input type="text" name="albumTitle" id="albumTitle" value="${formState.albumTitle}" class="trackInput" />
+      </p>
+      <p>
+        <label for="artistName" class="db">Artist Name</label>
+        <input type="text" name="artistName" id="artistName" value="${formState.artistName}" class="trackInput" />
+      </p>
+      <input type="submit" value="${formState.id ? "Edit Album" : "Add Album"}" />
+    </form>
+  `
+}
 const renderList = (albums) => {
   return albums.map(function(album){
     return `
@@ -8,57 +39,105 @@ const renderList = (albums) => {
         class="db bg-center cover aspect-ratio--object" />
       </div>
       <a href="#0" class="ph2 ph0-ns pb3 link db">
-        <h3 class="f5 f4-ns mb0 black-90">${album.title}</h3>
-        <h3 class="f6 f5 fw4 mt2 black-60">${album.artist}</h3>
+        <h3 class="f5 f4-ns mb0 black-90">${album.albumTitle}</h3>
+        <h3 class="f6 f5 fw4 mt2 black-60">${album.artistName}</h3>
       </a>
+      <p><button class="editAlbum" data-id="${album.id}">Edit Album</button></p>
     </article>
     `
   }).join("")
 }
 
-// establish link to our html
+// getting DOM nodes
 function getRoot() {
   return document.getElementById('root')
 }
 
-function getForm() {
-  return document.getElementById('addAlbum')
+function getFormContainer() {
+  return document.getElementById('form')
+}
+
+// updating the DOM
+function updateForm(formState) {
+  getFormContainer().innerHTML = renderForm(formState)
+  // tracks form State on change of text field inputs
+  document.querySelectorAll('.trackInput').forEach(input => {
+    input.addEventListener('change', (e) => {
+      UIState.formState[e.target.name] = e.target.value
+    })
+  })
 }
 
 function updateAlbumList(albums) {
   getRoot().innerHTML = renderList(albums)
+  document.querySelectorAll('.editAlbum').forEach(button => {
+    button.addEventListener('click', (e) => {
+      let editingAlbum = UIState.albums.find(album => album.id == e.target.dataset.id)
+      UIState.formState = editingAlbum
+      UIState.editingAlbumId = editingAlbum.id
+      updateAlbumList(UIState.albums.filter(album => album.id == e.target.dataset.id))
+      updateForm(UIState.formState)
+    })
+  })
 }
+
+// User Actions/Event Listeners updating UI state and updating the DOM (using pieces of that UI state as arguments)
 
 window.addEventListener('DOMContentLoaded', (event) => {
   
   console.log('DOM fully loaded and parsed');
+  // adds form to the DOM
+  updateForm(UIState.formState)
+  
   fetch("http://localhost:3000/albums")
     .then(res => res.json())
     .then(json => {
-      albums = json
-      updateAlbumList(albums)
+      UIState.albums = json
+      updateAlbumList(UIState.albums)
     })
-  getForm().addEventListener('submit', (event) => {
+  getFormContainer().addEventListener('submit', (event) => {
     event.preventDefault();
-    let artistName = event.target.querySelector('#artistName').value
-    let coverArt = event.target.querySelector('#coverArt').value
-    let title = event.target.querySelector('#title').value
-    let album = {
-      coverArt: coverArt,
-      title: title,
-      artist: artistName
+    if(UIState.editingAlbumId) {
+      updateAlbum(UIState.formState)
+    } else {
+      createAlbum(UIState.formState)
     }
-    fetch("http://localhost:3000/albums", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(album)
-    })
-      .then(res => res.json())
-      .then(json => {
-        albums.push(json)
-        updateAlbumList(albums)
-      })
   })
 });
+
+function createAlbum(album) {
+  fetch("http://localhost:3000/albums", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(album)
+  })
+    .then(res => res.json())
+    .then(json => {
+      UIState.albums.push(json)
+      updateAlbumList(UIState.albums)
+    })
+}
+
+function updateAlbum(album) {
+  debugger
+  fetch(`http://localhost:3000/albums/${album.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(album)
+  })
+    .then(res => res.json())
+    .then(json => {
+      UIState.albums = UIState.albums.map(album => {
+        if(album.id === json.id) {
+          return json
+        } else {
+          return album
+        }
+      })
+      updateAlbumList(UIState.albums)
+    })
+}
